d7c87b6d5a6bb0742c2967920ff9e0b744f65145
aaron44-YX <aaronporlas140@gmail.com>
2025-04-09 11:10:51 -0800

    freeshipping page adjustments

--- components/free_shipping_page.php
+++ components/free_shipping_page.php
@@ -12,22 +12,22 @@
     </div>
     
     <h1 class="text-3xl font-bold text-white text-center">How to get free shipping</h1>
-    <div class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-8 place-items-center p-6 mx-10">
-    <div class="bg-white h-48 p-4 flex flex-col justify-center items-center ">
-        <img src="../img/freeshiping.png" alt="Free Shipping" class="w-40 h-auto object-contain">
-        <h5>Set your delivery address</h5>
+<div class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-8 place-items-center p-6 mx-10">
+    <div class="bg-white h-60 p-4 flex flex-col justify-center items-center ">
+        <img src="../img/istockphoto-635997568-612x612.jpg" alt="Free Shipping" class="w-40 h-auto object-contain">
+        <h5 class="text-gray-700">Set your delivery address</h5>
     </div>
-    <div class="bg-white h-48 p-4 flex flex-col justify-center items-center ">
-        <img src="../img/freeshiping.png" alt="Free Shipping" class="w-40 h-auto object-contain">
-        <h5>Qualifying items shipped by Pearlas will display “FREE Shipping” by item price</h5>
+    <div class="bg-white h-60 p-4 flex flex-col justify-center items-center ">
+        <img src="../img/large-set-of-household-items-and-toys-vector.jpg" alt="Free Shipping" class="w-40 h-auto object-contain">
+        <h5 class="text-gray-700">Qualifying items shipped by Pearlas will display “FREE Shipping” by item price</h5>
     </div>
-    <div class="bg-white h-48 p-4 flex flex-col justify-center items-center ">
-        <img src="../img/freeshiping.png" alt="Free Shipping" class="w-40 h-auto object-contain">
-        <h5>Review your Shopping Cart to see if your order qualifies for FREE Shipping</h5>
+    <div class="bg-white h-60 p-4 flex flex-col justify-center items-center ">
+        <img src="../img/png-transparent-high-quality-freight-package-stacks-flowers-delivery-city-express-parcel-transport-thumbnail.png" alt="Free Shipping" class="w-40 h-auto object-contain">
+        <h5 class="text-gray-700">Review your Shopping Cart to see if your order qualifies for FREE Shipping</h5>
     </div>
-    <div class="bg-white h-48 p-4 flex flex-col justify-center items-center ">
-        <img src="../img/freeshiping.png" alt="Free Shipping" class="w-40 h-auto object-contain">
-        <h5>If your order qualifies, “FREE PearlasGlobal Shipping” will be pre-selected at checkout</h5>
+    <div class="bg-white h-60 p-4 flex flex-col justify-center items-center ">
+        <img src="../img/truck-globe-4245894.webp" alt="Free Shipping" class="w-40 h-auto object-contain">
+        <h5 class="text-gray-700">If your order qualifies, “FREE PearlasGlobal Shipping” will be pre-selected at checkout</h5>
     </div>
 </div>
 
