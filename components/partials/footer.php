10c34b290034bfcf94895b6abb3a409068c5ca7d
aaron44-YX <aaronporlas140@gmail.com>
2025-04-10 21:07:02 -0800

    footer done

--- components/partials/footer.php
+++ components/partials/footer.php
@@ -1,8 +1,22 @@
-<footer class="h-64 bg-white">
+<footer class="h-64 bg-white grid md:grid-cols-3">
         <div class="py-10 px-10 ">
-            <h1 class="text-xl text-gray-700 font-bold">Pearlas</h1>
+            <h1 class="text-2xl text-gray-700 font-bold">Pearlas</h1><br>
+            <p class="text-gray-700 text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolorum reiciendis distinctio voluptatibus corporis atque
+             ipsum aspernatur pariatur laboriosam!</p>
         </div>
-    </footer>
+        <div class="py-10 px-10 flex flex-col gap-4">
+            <h1 class="text-gray-700 text-lg">Keep Connected</h1>
+            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-facebook" class="text-gray-700 text-xl"></ion-icon><h1>Like us on Facebook</h1></a>
+            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-instagram" class="text-gray-700 text-xl"></ion-icon><h1>Check me out on Instagram</h1></a>
+            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-twitter" class="text-gray-700 text-xl"></ion-icon><h1>Follow me on Twitter</h1></a>
+            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-linkedin" class="text-gray-700 text-xl"></ion-icon><h1>Contact me on Linkedin</h1></a> 
+        </div>
+        <div class="py-10 px-10 flex flex-col gap-4 text-gray-700">
+            <h1>Contact Information</h1>
+            +63 912 345 6789<br>
+            aaronporlas@gmail.com
+        </div>
+</footer>
 
     <script src="script.js"></script>
 </body>
