6e5c49b4e7f0ff92bb2fc0aa4836e4fbd71b4636
aaron44-YX <aaronporlas140@gmail.com>
2025-04-17 09:38:01 -0800

    footer responsive update

--- components/partials/footer.php
+++ components/partials/footer.php
@@ -1,23 +1,35 @@
-<footer class="h-64 bg-white grid md:grid-cols-3">
-        <div class="py-10 px-10 ">
-            <h1 class="text-2xl text-gray-700 font-bold">Pearlas</h1><br>
-            <p class="text-gray-700 text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolorum reiciendis distinctio voluptatibus corporis atque
-             ipsum aspernatur pariatur laboriosam!</p>
-        </div>
-        <div class="py-10 px-10 flex flex-col gap-4">
-            <h1 class="text-gray-700 text-lg">Keep Connected</h1>
-            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-facebook" class="text-gray-700 text-xl"></ion-icon><h1>Like us on Facebook</h1></a>
-            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-instagram" class="text-gray-700 text-xl"></ion-icon><h1>Check me out on Instagram</h1></a>
-            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-twitter" class="text-gray-700 text-xl"></ion-icon><h1>Follow me on Twitter</h1></a>
-            <a href="#" class="text-gray-700 flex flex-row gap-2 items-center hover:underline hover:underline-offset-4"><ion-icon name="logo-linkedin" class="text-gray-700 text-xl"></ion-icon><h1>Contact me on Linkedin</h1></a> 
-        </div>
-        <div class="py-10 px-10 flex flex-col gap-4 text-gray-700">
-            <h1>Contact Information</h1>
-            +63 912 345 6789<br>
-            aaronporlas@gmail.com
-        </div>
-</footer>
+<footer class="bg-white grid gap-6 p-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+  <div>
+    <h1 class="text-2xl text-gray-700 font-bold">Pearlas</h1>
+    <p class="mt-4 text-gray-700 text-sm">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolorum reiciendis distinctio voluptatibus corporis atque
+      ipsum aspernatur pariatur laboriosam!
+    </p>
+  </div>
 
-    <script src="script.js"></script>
-</body>
-</html>
+  <div class="flex flex-col gap-4">
+    <h1 class="text-gray-700 text-lg font-semibold">Keep Connected</h1>
+    <a href="#" class="text-gray-700 flex items-center gap-2 hover:underline hover:underline-offset-4">
+      <ion-icon name="logo-facebook" class="text-xl"></ion-icon>
+      <span>Like us on Facebook</span>
+    </a>
+    <a href="#" class="text-gray-700 flex items-center gap-2 hover:underline hover:underline-offset-4">
+      <ion-icon name="logo-instagram" class="text-xl"></ion-icon>
+      <span>Check me out on Instagram</span>
+    </a>
+    <a href="#" class="text-gray-700 flex items-center gap-2 hover:underline hover:underline-offset-4">
+      <ion-icon name="logo-twitter" class="text-xl"></ion-icon>
+      <span>Follow me on Twitter</span>
+    </a>
+    <a href="#" class="text-gray-700 flex items-center gap-2 hover:underline hover:underline-offset-4">
+      <ion-icon name="logo-linkedin" class="text-xl"></ion-icon>
+      <span>Contact me on Linkedin</span>
+    </a> 
+  </div>
+
+  <div class="flex flex-col gap-2 text-gray-700">
+    <h1 class="text-lg font-semibold">Contact Information</h1>
+    <p>+63 912 345 6789</p>
+    <p>aaronporlas@gmail.com</p>
+  </div>
+</footer>
