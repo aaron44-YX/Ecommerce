d2614a50e205784dd46c0ab3b8dd85f3f4553353
aaron44-YX <aaronporlas140@gmail.com>
2025-04-25 03:48:17 -0800

    update the orders page

--- admin_components/orders.php
+++ admin_components/orders.php
@@ -9,29 +9,10 @@
     
     <table class="w-full border border-gray-300 text-left rounded-xl overflow-hidden border-separate border-spacing-0 ">
         <thead >
-            <tr>
-                <th class="font-semibold p-2 border-b border-gray-300">Date</th>
-                <th class="font-semibold p-2 border-b border-gray-300">Customer</th>
-                <th class="font-semibold p-2 border-b border-gray-300">Address</th>
-                <th class="font-semibold p-2 border-b border-gray-300">Nb Items</th>
-                <th class="font-semibold p-2 border-b border-gray-300">Total</th>
-            </tr>
+            
         </thead>
         <tbody>
-            <tr>
-                <td class="p-2">12/12/12, 8:50 AM</td>
-                <td class="p-2">Aaron Porlas</td>
-                <td class="p-2">Lacion,jugan,antipolo,cebuqwewqeqweqweqweqwewqeqweqwe</td>
-                <td class="p-2">2</td>
-                <td class="p-2">90</td>
-            </tr>
-            <tr>
-                <td class="p-2">12/12/12, 8:50 AM</td>
-                <td class="p-2">Aaron Porlas</td>
-                <td class="p-2">Lacion,jugan,antipolo,cebuqwewqeqweqweqweqwewqeqweqwe</td>
-                <td class="p-2">2</td>
-                <td class="p-2">90</td>
-            </tr>
+            
         </tbody>
     </table>
 
