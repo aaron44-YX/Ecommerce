7e0944458542fd4bc53bacd2f4dff5fa8d14d7ec
aaron44-YX <aaronporlas140@gmail.com>
2025-05-07 05:08:57 -0800

    fixed the footer

--- user_profile.php
+++ user_profile.php
@@ -1,10 +1,12 @@
 <?php require('./components/partials/head.php')?>
 <?php require('./components/partials/nav.php')?>
 
+<main class="flex-grow">
 <div class="flex justify-end items-center">
 <a href="logout.php" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded inline-block">
     Logout
 </a>
 </div>
+</main>
 
 <?php require('./components/partials/footer.php')?>
