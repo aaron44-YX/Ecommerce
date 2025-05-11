eb4143d9630613b1277c3f0a8e010b4ce7f1d9b3
aaron44-YX <aaronporlas140@gmail.com>
2025-05-11 02:27:32 -0800

    added a cart count on the cart icon

--- components/partials/cart.php
+++ components/partials/cart.php
@@ -1,3 +1,20 @@
-<a href="/ecommerce_project/cart_page.php">
+<?php
+require_once __DIR__ . '/../../session_start.php';
+$cartCount = 0;
+
+if (!empty($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $item) {
+        $cartCount += $item['quantity'];
+    }
+}
+?>
+
+<a href="/ecommerce_project/cart_page.php" class="relative inline-block">
   <ion-icon name="cart" class="w-6 h-6 text-green-600 cursor-pointer"></ion-icon>
+  <?php if ($cartCount > 0): ?>
+    <span class="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full px-1.5">
+      <?= $cartCount ?>
+    </span>
+  <?php endif; ?>
 </a>
+
